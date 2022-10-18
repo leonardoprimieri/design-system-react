2fb26c843aef837e28a84a160d7a8b7fb970ccf0
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-10-18 16:36:23 -0300

    feat: create TokensGrid for every token

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import '../styles/tokens-grid.css'
 
-type TokenGridProps = {
+interface TokensGridProps {
   tokens: Record<string, string>
+  hasRemValue?: boolean
 }
 
-export function TokensGrid({ tokens }: TokenGridProps) {
+export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProps) {
   return (
     <table className="tokens-grid">
       <thead>
-        <tr>Name</tr>
-        <tr>Value</tr>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          {hasRemValue && <th>Pixels</th>}
+        </tr>
       </thead>
+
       <tbody>
-        {Object.entries(tokens).map(([name, value]) => (
-          <tr key={value}>
-            <td>{name}</td>
-            <td>{value}</td>
-          </tr>
-        ))}
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+              {hasRemValue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   )
