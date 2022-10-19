101019cf5a554b71c2802926941ce258d59b5264
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-10-19 10:18:04 -0300

    feat: add Box component

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -20,7 +20,7 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
-  Button: () => Button
+  Box: () => Box
 });
 module.exports = __toCommonJS(src_exports);
 
@@ -127,32 +127,14 @@ var {
   }
 });
 
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite300",
-  borderRadius: "$sm",
-  padding: "$2 $4",
-  border: 0,
-  fontWeight: "$bold",
-  color: "$white",
-  variants: {
-    size: {
-      small: {
-        fontSize: "$sm",
-        padding: "$2 $4"
-      },
-      big: {
-        fontSize: "$lg",
-        padding: "$3 $6"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
+// src/components/Box.tsx
+var Box = styled("div", {
+  padding: "$4",
+  borderRadius: "$md",
+  backgroundColor: "$gray800",
+  border: "1px solid $gray600"
 });
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  Button
+  Box
 });
