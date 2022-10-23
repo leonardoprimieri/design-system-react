6478e6b0cac45e55f3cec43dbf04ea790a87ce09
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-10-22 21:45:30 -0300

    feat: add more control types

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -6,6 +6,25 @@ export default {
   component: Heading,
   args: {
     children: 'Custom title',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '3xl',
+        '4xl',
+        '5xl',
+        '6xl',
+      ],
+      control: { type: 'inline-radio' },
+    },
   },
 } as Meta<HeadingProps>
 
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,28 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sequi facilis amet excepturi saepe enim temporibus, mollitia dolorum sint repellat error quidem recusandae aperiam eum fuga ab natus numquam cum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '3xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: { type: 'inline-radio' },
+    },
   },
 } as Meta<TextProps>
 
